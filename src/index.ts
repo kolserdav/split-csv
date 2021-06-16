9a251a961e062a273371cc2b8a1c2990655bde9b
kolserdav <serega12101983@gmail.com>
2021-06-16 19:41:49 +0700

    change .gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 
-const sourcePath = path.resolve(__dirname, '../data/1.csv');
-const distPath = path.resolve(__dirname, '../result');
+const sourcePath = path.resolve(__dirname, '../data/2.csv');
+const distPath = path.resolve(__dirname, '../result2');
 
 const stream = fs.createReadStream(sourcePath);
 let i = 0;
@@ -11,7 +11,7 @@ stream.on('data', (buff) => {
   const dataStr = buff.toString();
   const dsplit = dataStr.split(/"\d{7}"/)
   const tail = dataStr.match(/\d{7}[.\s\S$]+/);
-  if (i % 8000 === 0) {
+  if (i % 2875 === 0) {
     fileName = `${i}.csv`;
   }
   fs.appendFileSync(`${distPath}/${fileName}`, dataStr);
